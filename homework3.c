e4225159c1b26c849297792af24f69ee1508aa49
Sean <s_moloney@aol.com>
2018-04-09 13:42:24 -0700

    Initial Commit

---
+++ homework3.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int main(int argc, char* argv[]){
+    int multiArray[2][2] = {{0, 1, 2}, {8, 9, 10}};
+    
+
+}
